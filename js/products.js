79abc9bf1e2d0a3c68c45022f365a3e400cdf7af
Agustina 1. Ramos <agustina.1.ramos@visitor.upm.com>
2022-09-03 23:25:17 -0300

    all

--- js/products.js
+++ js/products.js
@@ -5,7 +5,7 @@ let categoriesArray = []
 function showCategoriesList(autos){
     let htmlContentToAppend = "";
 
-    for(let auto of autos.products){ 
+    for(let auto of autos){ 
       
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
@@ -35,8 +35,85 @@ function showCategoriesList(autos){
         getJSONData(dire1).then(function(resultObj){
             if (resultObj.status === "ok")
             {
-                categoriesArray = resultObj.data;
+                categoriesArray = resultObj.data.products;
                 showCategoriesList(categoriesArray);
             }
         });
+        document.getElementById("filtrar").addEventListener("click", function(){
+            filtrar();
+        });
+        document.getElementById("sortAsc").addEventListener("click", function(){
+            let result = [];
+            result = categoriesArray.sort(function(a, b) {
+                if ( a.cost < b.cost ){ return -1; }
+                if ( a.cost > b.cost ){ return 1; }
+                return 0;
+            });
+            showCategoriesList(result);
+        });
+    
+        document.getElementById("sortDesc").addEventListener("click", function(){
+            let result = [];
+            result = categoriesArray.sort(function(a, b) {
+                if ( a.cost > b.cost ){ return -1; }
+                if ( a.cost < b.cost ){ return 1; }
+                return 0;
+            });
+            showCategoriesList(result);
+        });
+    
+        document.getElementById("sortByCount").addEventListener("click", function(){
+            let result = [];
+            result = categoriesArray.sort(function(a, b) {
+                let aCount = parseInt(a.soldCount);
+                let bCount = parseInt(b.soldCount);
+    
+                if ( aCount > bCount ){ return -1; }
+                if ( aCount < bCount ){ return 1; }
+                return 0;
+            });
+            showCategoriesList(result);
+        });
     });
+
+    function filtrar(){
+        //parseInt porque es un string, y necesito un integer
+        let inicial = parseInt(document.getElementById('minimo').value);//tomo el valor mínimo
+        let final = parseInt(document.getElementById('maximo').value);//tomo el valor máximo
+        let listaFiltrada = categoriesArray.filter(producto => producto.cost >= inicial && producto.cost <= final );
+        // arr.sort((a,b)=>a-b)
+        listaFiltrada.sort((ant,sig)=>ant.cost-sig.cost);
+      
+        showCategoriesList (listaFiltrada);
+    
+    }  
+
+    function sortCategories(criteria, array){
+        let result = [];
+        if (criteria === ORDER_ASC_BY_NAME)
+        {
+            result = array.sort(function(a, b) {
+                if ( a.cost < b.cost ){ return -1; }
+                if ( a.cost > b.cost ){ return 1; }
+                return 0;
+            });
+        }else if (criteria === ORDER_DESC_BY_NAME){
+            result = array.sort(function(a, b) {
+                if ( a.cost > b.cost ){ return -1; }
+                if ( a.cost < b.cost ){ return 1; }
+                return 0;
+            });
+        }else if (criteria === ORDER_BY_PROD_COUNT){
+            result = array.sort(function(a, b) {
+                let aCount = parseInt(a.soldCount);
+                let bCount = parseInt(b.soldCount);
+    
+                if ( aCount > bCount ){ return -1; }
+                if ( aCount < bCount ){ return 1; }
+                return 0;
+            });
+        }
+    
+        return result;
+    }
+
